426b1349508c973e6be1f8d65d7afc9536584951
NewoIsTaken <owenwang2007@icloud.com>
2020-07-11 23:32:23 -0400

    Name changing

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -16,7 +16,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('COVID Contact Tracer'),
+        title: Text('COVID-19 Contact Tracker'),
       ),
       //body: _children[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
